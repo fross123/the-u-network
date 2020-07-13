a6414e6f9561e2602c512131932df8ff8f950233
Finlay <rossf.seg@gmail.com>
2020-07-13 16:36:52 -0500

    more tests

--- network/tests.py
+++ network/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import authenticate
 
-from .models import User
+from .models import User, Post
 
 # Create your tests here.
 class NetworkTestCase(TestCase):
@@ -9,6 +9,12 @@ class NetworkTestCase(TestCase):
         # Create Test Users
         u1 = User.objects.create_user("AAA", "AAA@test.com", "AAA")
         u2 = User.objects.create_user("BBB", "BBB@test.com", "BBB")
+
+        # Create test posts
+        Post.objects.create(content="Fou", user=u1, likes=0)
+        Post.objects.create(content="Fou2", user=u2, likes=0)
+        Post.objects.create(content="Fou3", user=u2, likes=0)
+        Post.objects.create(content="Fou4", user=u2, likes=0)
 
     def testAuthentication(self):
         """ Test if User can be authenticated """
@@ -20,3 +26,8 @@ class NetworkTestCase(TestCase):
         """ Test if user is Denied """
         user2 = authenticate(self, username="BBB", password="ABCD")
         self.assertEqual(user2, None)
+
+    def testPosting(self):
+        """ Test if post can be created """
+        u = authenticate(self, username="BBB", password="BBB")
+        self.assertEqual(u.posts.count(), 3)

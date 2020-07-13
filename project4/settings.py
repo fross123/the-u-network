0726532d70f5f4d07c164335a7fbc5aa38dc7cda
Finlay <rossf.seg@gmail.com>
2020-07-13 14:52:21 -0500

    back to sqlLite

--- project4/settings.py
+++ project4/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Project4
+![Django Tests](https://github.com/fross123/cs50w_project4/workflows/Django%20Tests/badge.svg)

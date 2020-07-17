3d7f5b4d81217ac34d7821c10241293152af5ac8
Finlay <rossf.seg@gmail.com>
2020-07-16 21:22:38 -0500

    trying to get these tests to work

--- project4/settings.py
+++ project4/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
